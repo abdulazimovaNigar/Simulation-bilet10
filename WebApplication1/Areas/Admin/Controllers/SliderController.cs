bc2c8735614f4294df005352730d02b26748a077
abdulazimovaNigar <nigarna-mpa201@code.edu.az>
2026-01-12 19:13:26 +0400

    CRUD

--- WebApplication1/Areas/Admin/Controllers/SliderController.cs
+++ WebApplication1/Areas/Admin/Controllers/SliderController.cs
@@ -15,8 +15,54 @@ public class SliderController(AppDbContext _context) : Controller
         return View(sliders);
     }
 
-    /*public IActionResult Read() 
+    [HttpGet]
+    public IActionResult Create() 
     { 
-        
-    }*/
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Create(Slider slider)
+    {
+        if (!ModelState.IsValid) return View(slider);
+
+        _context.Sliders.Add(slider);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpGet]
+    public IActionResult Update(int id) 
+    {
+        var slider = _context.Sliders.FirstOrDefault(s => s.Id == id);
+        if (slider == null) return NotFound();
+        return View(slider);
+    }
+
+    [HttpPost]
+    public IActionResult Update(Slider slider) 
+    { 
+        if (!ModelState.IsValid) return View(slider);
+        var existSlider = _context.Sliders.FirstOrDefault(s => s.Id == slider.Id);
+
+        if (existSlider == null) return NotFound();
+        existSlider.Title = slider.Title;
+        existSlider.Description = slider.Description;
+        existSlider.ImageURL = slider.ImageURL;
+
+
+        _context.Sliders.Update(existSlider);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    public IActionResult Delete(int id) 
+    { 
+        var slider = _context.Sliders.FirstOrDefault(s => s.Id == id);
+        if (slider == null) return NotFound();
+
+        _context.Sliders.Remove(slider);
+        _context.SaveChanges();
+        return RedirectToAction(nameof(Index));
+    }
 }
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2cc778a832e481e73e9187f2066585252b3bd6f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
